836fb194a530d7d4fa6119913162d32d5461851d
Pierre Desmarchelier <desmarchelier.p21@gmail.com>
2020-03-02 14:30:39 +0100

    add of csv support

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
+#encoding : utf-8
+
 from flask import Flask, request, jsonify
 import os
+import csv
 
 #Init app
 app = Flask(__name__)
 
 metadonne={}
 jsonoutput={}
+ALLOWED_EXTENSIONS = {'txt', 'csv'} # 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 @app.route("/transform", methods=['POST'])
 def upload_file():
     if request.files['data_file']:
         filetodisplay = request.files['data_file']
-        if filetodisplay.mimetype == 'text/plain':
-            metadonne['mimetype'] = filetodisplay.mimetype
-            metadonne['filename'] = filetodisplay.filename
-            datafile = filetodisplay.read().decode('utf-8').split("\n")
-            jsonoutput['Metadonnees'] = metadonne
-            jsonoutput['Data'] = datafile
-            return jsonify(jsonoutput)
+        if filetodisplay.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+            if filetodisplay.mimetype == 'text/plain':
+                metadonne['mimetype'] = filetodisplay.mimetype
+                metadonne['filename'] = filetodisplay.filename
+                datafile = filetodisplay.read().decode('utf-8').splitlines()
+                jsonoutput['Metadonnees'] = metadonne
+                jsonoutput['Data'] = datafile
+                return jsonify(jsonoutput)
+
+            elif filetodisplay.mimetype == 'text/csv':
+                # ',' must be the delimiter of the csv input
+                metadonne['mimetype'] = filetodisplay.mimetype
+                metadonne['filename'] = filetodisplay.filename
+                fileString = filetodisplay.read().decode('utf-8')
+                datafile = [{k: v for k, v in row.items()} for row in csv.DictReader(fileString.splitlines(), skipinitialspace=True)]
+                jsonoutput['Metadonnees'] = metadonne
+                jsonoutput['Data']=datafile
+                return jsonify(jsonoutput)
         else:
-            return 'The mimetype of your file is not yet supported'
+            return 'The mimetype of your file is not yet supported' 
     else:
-        return 'Please enclose a file (only POST Request accepted for the moment)'
+        return 'Please enclose a file'
 
 
 #Run server 
