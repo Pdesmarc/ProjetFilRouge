f46d30553d403cef08212e15ce31f04f755d00af
Pierre Desmarchelier <desmarchelier.p21@gmail.com>
2020-03-02 10:16:53 +0100

    add of first mimetype accepted : txt

--- app.py
+++ app.py
@@ -4,9 +4,25 @@ import os
 #Init app
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
-def get() : 
-    return jsonify({'init': 'test flask server and postman'})
+metadonne={}
+jsonoutput={}
+
+@app.route("/transform", methods=['POST'])
+def upload_file():
+    if request.files['data_file']:
+        filetodisplay = request.files['data_file']
+        if filetodisplay.mimetype == 'text/plain':
+            metadonne['mimetype'] = filetodisplay.mimetype
+            metadonne['filename'] = filetodisplay.filename
+            datafile = filetodisplay.read().decode('utf-8').split("\n")
+            jsonoutput['Metadonnees'] = metadonne
+            jsonoutput['Data'] = datafile
+            return jsonify(jsonoutput)
+        else:
+            return 'The mimetype of your file is not yet supported'
+    else:
+        return 'Please enclose a file (only POST Request accepted for the moment)'
+
 
 #Run server 
 if  __name__=='__main__':
