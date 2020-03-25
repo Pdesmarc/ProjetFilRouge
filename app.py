dd92f4fb68515611b294f61b18a53dbb41cf7e70
Pierre Desmarchelier <desmarchelier.p21@gmail.com>
2020-03-25 23:04:26 +0100

    Add of comments and docstrings

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-"""
-Add to README
-Be sure to be on sls_branch
-Run 
-    sls plugin install -n serverless-wsgi
-    sls plugin install -n serverless-python-requirements
-Deploy
-    sls deploy
-"""
 #encoding : utf-8
 import boto3
 import argparse
@@ -41,11 +32,28 @@ jsonoutput={}
 ALLOWED_EXTENSIONS = {'txt', 'csv', 'jpg', 'jpeg' ,'pdf', 'png'} #, 'gif'}
 
 def allowed_file(filename):
+    ''' Check if the file respond to our in input criteria
+        Args:
+            filename (string): The name of the input file
+           
+
+        Returns:
+            bool: True if the file is allowed to be processed, false if not
+        '''
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def get_metadata(file):
+    '''Retrieve three metadatas for the input file 
+
+        Args:
+            file (FileStorage): The input file
+
+
+        Returns:
+            metadate(dictionnary): A dict containing the filename, the mimetype and the size
+        '''
     metadata={}
     metadata['mimetype'] = file.mimetype
     metadata['filename'] = file.filename
@@ -54,6 +62,15 @@ def get_metadata(file):
     return metadata
 
 def upload_to_s3(datafile, filename):
+    '''Upload the file to s3
+        Args:
+            datafile (FileStorageStream): The input file stream
+            filename (string) : The name of the input file
+
+
+        Returns:
+            nothing
+        '''
     f =  BytesIO(datafile)
     s3 = boto3.client('s3')
     s3.upload_fileobj(f, 'iaas-projetfilrouge-pdesmarc', filename)
@@ -63,9 +80,11 @@ def upload_to_s3(datafile, filename):
 
 @app.route("/transform", methods=['POST'])
 def upload_file():
+    #Return an error if no file submitted
     if not request.files : 
         return jsonify({'Error ' : 'Please enclose a file'}), 400
 
+    #Return an error if the file is not submitted with a parameter called updfile
     if 'upfile' not in request.files:
         return jsonify({'Error ' : 'In the POST request, the key parameter must be called \'upfile\' '}), 400
 
@@ -83,25 +102,29 @@ def upload_file():
         upload_to_s3(datafile, filetodisplay.filename)
 
         # Retrieve data, specific treatment in function of mimetype file 
-        if filetodisplay.mimetype == 'text/plain':                                      
+        if filetodisplay.mimetype == 'text/plain':  
+            #Retrieve each file of the text file                                       
             datafile = datafile.decode('utf-8').splitlines()
             jsonoutput['Data'] = datafile
             
 
         elif filetodisplay.mimetype == 'text/csv':
-            # ',' must be the delimiter of the csv input, try csv.sniffer if you want to use different delimiter 
+            # ',' must be the delimiter of the csv input
             fileString = datafile.decode('utf-8')
+            # Retrieve each data with its column associated
             datafile = [{k: v for k, v in row.items()} for row in csv.DictReader(fileString.splitlines(), skipinitialspace=True)]
             jsonoutput['Data']=datafile
 
-        else:           
+        else:
+            #For image-like file, we encode the data to Base64. A simply decode64 permit to retrieve the file as submitted           
             data_string = base64.b64encode(datafile)
             jsonoutput['Data']=data_string.decode('utf-8')
             
         return jsonify(jsonoutput)
-
+    #If there is no file submitted even if a parameter called 'upfile' is sent, an error is sent back
     elif not filetodisplay :
         return jsonify({'Error ' : 'Please enclose a file'}), 400
+        #If the mimetype is not yet supported, an error is sent back
     else:
         return jsonify({'Error' : 'The mimetype of your file is not yet supported'}), 400
 
